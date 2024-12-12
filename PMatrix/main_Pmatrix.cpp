e4243f4595d17d700bcfb0670ef55bc79705db50
nadahkhaledd <nadahkhaledd@gmail.com>
2024-12-12 22:59:07 +0100

    integrated main to run based on argument flags

--- PMatrix/main_Pmatrix.cpp
+++ PMatrix/main_Pmatrix.cpp
@@ -2,59 +2,53 @@
 #include <string>
 #include <mpi.h>
 #include "MeryPmatrix.hpp"
-#include "sparsePMatrix.hpp"
+#include "sparseMatrix.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    // Initialize MPI
-    MPI_Init(&argc, &argv);
-
-    int mpi_rank, mpi_size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 
     // Check for implementation flag
     if (argc < 2) {
-        if (mpi_rank == 0) {
-            cerr << "Usage: " << argv[1] << " <implementation_flag> [matrix_file.mtx]" << endl;
-            cerr << "Implementation flags: --dense or --sparse" << endl;
-        }
-        MPI_Abort(MPI_COMM_WORLD, 1);
+        cerr << "Usage: " << argv[1] << " <implementation_flag> [matrix_file.mtx]" << endl;
+        cerr << "Implementation flags: --dense or --sparse" << endl;
+        return 1;
     }
 
     string flag = argv[1];
 
     if (flag == "--dense") {
-        cout << "Implementing with Dense matrix\n";
+        // Initialize MPI
+        MPI_Init(&argc, &argv);
+
+        int mpi_rank, mpi_size;
+        MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
+
         // Run the dense matrix implementation
         MatrixTest matrix_test;
         matrix_test.initialize(mpi_rank);
         matrix_test.run_tests(mpi_rank, mpi_size);
+
+        // Finalize MPI
+        MPI_Finalize();
     } else if (flag == "--sparse") {
-        cout << "Implementing with sparse matrix\n";
+        cout << "Implementing with sparse matrix in scalar mode\n";
         // Check for matrix file argument
         if (argc < 3) {
-            if (mpi_rank == 0) {
-                cerr << "Usage: " << argv << " --sparse <matrix_file.mtx>" << endl;
-            }
-            MPI_Abort(MPI_COMM_WORLD, 1);
+            cerr << "Usage: " << argv << " --sparse <matrix_file.mtx>" << endl;
+            return 1;
         }
 
         string matrix_file = argv[2];
 
         // Run the sparse matrix implementation
         SPMatrixTest matrix_test;
-        matrix_test.initialize(mpi_rank, matrix_file);
-        matrix_test.run_test(mpi_rank, mpi_size);
+        matrix_test.initialize(matrix_file);
+        matrix_test.run_test();
     } else {
-        if (mpi_rank == 0) {
-            cerr << "Invalid implementation flag. Use --dense or --sparse." << endl;
-        }
-        MPI_Abort(MPI_COMM_WORLD, 1);
+        cerr << "Invalid implementation flag. Use --dense or --sparse." << endl;
+        return 1;
     }
-
-    // Finalize MPI
-    MPI_Finalize();
     return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MatVet-CG-feature-3
 linear method solution with conjugate gradient to a sparse matrix.
+
+### to run:
+
+#### for feature-2: 
+
+```
+mpirun --use-hwthread-cpus --allow-run-as-root ./main_Pmatrix --dense
+```
+
+#### for feature-3:
+
+```
+./main_Pmatrix --sparse spd_test.mtx
+```
