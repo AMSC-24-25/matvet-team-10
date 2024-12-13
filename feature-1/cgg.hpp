71a85a0a4cab2eaa6ce69f957ce083e8dcc6378f
nadahkhaledd <nadahkhaledd@gmail.com>
2024-12-13 09:19:00 +0100

    updated cgg.hpp

--- feature-1/cgg.hpp
+++ feature-1/cgg.hpp
@@ -1,10 +1,26 @@
 #ifndef HH_GC___HH
 #define HH_GC___HH
 
-//***********************
-// Authors: Milica, Nada
+//*****************************************************************
+// Iterative template routine -- CG
 //
-//***********************
+// CG solves the symmetric positive definite linear
+// system Ax=b using the Conjugate Gradient method.
+//
+// CG follows the algorithm described on p. 15 in the
+// SIAM Templates book.
+//
+// The return value indicates convergence within max_iter (input)
+// iterations (0), or no convergence within max_iter iterations (1).
+//
+// Upon successful return, output arguments have the following values:
+//
+//        x  --  approximate solution to Ax = b
+// max_iter  --  the number of iterations performed before the
+//               tolerance was reached
+//      tol  --  the residual after the final iteration
+//
+//*****************************************************************
 
 #include <iostream>
 #include <vector>
